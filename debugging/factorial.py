c0a87a17b7558b96b1ac4595708a35db4d21098e
Abimael <icemask2612@gmail.com>
2024-05-06 16:32:12 -0400

    fix

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -2,21 +2,21 @@
 import sys
 
 def factorial(n):
-result = 1
-while n > 1:
-result *= n
-n = n - 1
-return result
+    result = 1
+    while n > 1:
+        result *= n
+        n = n - 1
+    return result
 
 if len(sys.argv) != 2:
-print("Usage: python script.py <number>")
-sys.exit(1)
+    print("Usage: python script.py <number>")
+    sys.exit(1)
 
 try:
-num = int(sys.argv[1])
+    num = int(sys.argv[1])
 except ValueError:
-print("Please provide a valid integer.")
-sys.exit(1)
+    print("Please provide a valid integer.")
+    sys.exit(1)
 
 f = factorial(num)
 print(f)                                                        
